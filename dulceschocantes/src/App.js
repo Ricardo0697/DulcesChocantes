ad6cb7dbb9a2b9a7aa0e91e83d5fe92a3af8de71
ItNairi <37809835+Ricardo0697@users.noreply.github.com>
2021-11-17 22:15:25 -0600

    Se Agrega el metodo para eliminar columnas y filas de colores

--- dulceschocantes/src/App.js
+++ dulceschocantes/src/App.js
@@ -4,7 +4,27 @@ const width = 8;
 const candyColors = ['blue', 'red', 'black', 'yellow', 'purple', 'orange', 'green', 'brown'];
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
- 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 40; i++) {
+      const columnOfFour = [i, i + width, i + width * 2 , i + width * 3]
+      const decidedColor = currentColorArrangement[i];
+      if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 65; i++) {
+      const rowOfThree = [i, i + 1 , i + 2]
+      const decidedColor = currentColorArrangement[i];
+      const noValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,64,65]
+      if(noValid.includes(i)) continue
+      if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
   const checkForColumnOfThree = () => {
     for (let i = 0; i < 48; i++) {
       const columnOfThree = [i, i + width, i + width * 2]
@@ -14,6 +34,8 @@ const App = () => {
       }
     }
   }
+
+
 
   const createBoard = () => {
     // crea el random de colores en el arreglo por medio de los colores de la lista de candycolors
@@ -35,12 +57,15 @@ const App = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour()
       checkForColumnOfThree()
+      checkForRowOfThree()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100);
     return () => clearInterval(timer)
 
-  }, [checkForColumnOfThree , currentColorArrangement])
+  }, [checkForColumnOfThree , checkForColumnOfFour , checkForRowOfThree , currentColorArrangement])
+  
   // createBoard()
   console.log(currentColorArrangement)
   return (
