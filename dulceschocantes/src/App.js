0c90859f70c67070e50cab0dd6a40949d00e34a2
ItNairi <37809835+Ricardo0697@users.noreply.github.com>
2021-11-30 09:58:19 -0600

    Update App.js

--- dulceschocantes/src/App.js
+++ dulceschocantes/src/App.js
@@ -60,6 +60,7 @@ const App = () => {
         for (let i = 0; i <= 47; i++) {
             const columnOfThree = [i, i + width, i + width * 2]
             const decidedColor = currentColorArrangement[i];
+            const isBlank = currentColorArrangement[i] === blank
             if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
                 setScoreDisplay((score) => score + 3)
                 columnOfThree.forEach(square => currentColorArrangement[square] = '')
@@ -67,13 +68,14 @@ const App = () => {
             }
         }
     }
-    
+
     const checkForRowOfThree = () => {
         for (let i = 0; i <= 64; i++) {
             const rowOfThree = [i, i + 1, i + 2]
             const decidedColor = currentColorArrangement[i];
-            const noValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 64]
-            if (noValid.includes(i)) continue
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+            const isBlank = currentColorArrangement[i] === blank
+            if (notValid.includes(i)) continue
             if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
                 setScoreDisplay((score) => score + 3)
                 rowOfThree.forEach(square => currentColorArrangement[square] = '')
