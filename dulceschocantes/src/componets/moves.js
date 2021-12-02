42065911332f45be06bb3bc18096dc0becabddfe
ItNairi <37809835+Ricardo0697@users.noreply.github.com>
2021-12-02 11:55:30 -0600

    Update moves.js

--- dulceschocantes/src/componets/moves.js
+++ dulceschocantes/src/componets/moves.js
@@ -1,9 +1,9 @@
 const Moves = ({ moves }) => {
-    return (
-      <div className="score-board styleMove">
-        <h2>Moves {moves}</h2>
-      </div>
-    )
-  }
-  
-  export default Moves
+  return (
+    <div className="score-board styleMove">
+      <h2>Moves {moves}</h2>
+    </div>
+  )
+}
+
+export default Moves
